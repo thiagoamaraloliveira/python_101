219a689d4b76af40c5f6d127a1d2a16a179768b8
thiago.ao <thiago.ao@samsung.com>
2023-05-10 14:46:52 -0300

    create project jogo

---
+++ 01- Exemplo inicial.py
@@ -0,0 +1,31 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> print
+<built-in function print>
+>>> Q
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Q
+NameError: name 'Q' is not defined
+>>> q
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    q
+NameError: name 'q' is not defined
+>>> q
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    q
+NameError: name 'q' is not defined
+>>> print("Brasil ganhou  títulos mundias")
+Brasil ganhou  títulos mundias
+>>> print("Brasil", "ganhou",5,"títulos mundias", sep="-")
+Brasil-ganhou-5-títulos mundias
+>>> quantidade = 4
+>>> pais ="Italia"
+>>> type(quantidade)
+<class 'int'>
+>>> print(pais,"ganhou",quantidade,"titulos mundiais")
+Italia ganhou 4 titulos mundiais
