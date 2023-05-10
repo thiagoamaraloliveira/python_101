09fb7af2514f4e6dd7348e0b46717d1c9d92473d
thiago.ao <thiago.ao@samsung.com>
2023-05-10 17:49:40 -0300

    verificação de erros e acertos

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,27 +4,33 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
-        chute = chute.strip()
-
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-
-            index = index + 1
+        chute = chute.strip().upper()
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-        print("jogando...")
-
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do Jogo")
 
 if(__name__ == "__main__"):
