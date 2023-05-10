ccfd64920f1cf763868adedaf19ee93e9ce47da0
thiago.ao <thiago.ao@samsung.com>
2023-05-10 16:29:45 -0300

    correção de import

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -48,3 +48,6 @@ def jogar():
 
     print("Fim de Jogo!!")
     print("**************")
+
+if(__name__ == "__main__"):
+    jogar()
--- jogos/jogos.py
+++ jogos/jogos.py
@@ -1,17 +1,21 @@
 import adivinhacao
 import forca
 
-print("*********************************")
-print("*****Escolha o seu Jogo!*********")
-print("*********************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("*****Escolha o seu Jogo!*********")
+    print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo?"))
+    jogo = int(input("Qual jogo?"))
 
-if(jogo == 1):
-    print("Jogando forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogando adivinhação")
-    adivinhacao.jogar()
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if (__name__ == "__main__"):
+    escolhe_jogo()
