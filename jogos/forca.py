a817b890697f6f1c6b060e37285e01e8f6259512
thiago.ao <thiago.ao@samsung.com>
2023-05-10 17:08:10 -0300

    criando o jogo forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,22 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index + 1
+        print("jogando...")
+
     print("Fim do Jogo")
 
 if(__name__ == "__main__"):
