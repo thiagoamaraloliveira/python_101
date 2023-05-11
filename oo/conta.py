251de3542123b15d51ec53bfc757f3a78ff8c68c
thiago.ao <thiago.ao@samsung.com>
2023-05-11 16:00:32 -0300

    encapsulamento

--- oo/conta.py
+++ oo/conta.py
@@ -1,2 +1,17 @@
 
-class
+class Conta:
+
+    def __int__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+    def saca(self, valor):
+        self.__saldo -= valor
